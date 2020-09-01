fa541766202dfb1217221c2f8083a2c50d4a15f1
rakutek <rakutek12@gmail.com>
2020-09-01 19:52:27 +0900

    date-fns使うのやめた

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import Date from "../components/date";
-
 import { getSortedPostsData } from "../lib/posts";
-
 import styled from "styled-components";
 
 const Light = styled.small`
