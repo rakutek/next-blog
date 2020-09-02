c643b5f52654df54a3bface75cd92fe9f1bbf3f1
rakutek <rakutek12@gmail.com>
2020-09-02 14:14:30 +0900

    GA導入できた

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,6 @@ import * as gtag from "../lib/gtag";
 
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
-import * as gtag from "lib/gtag";
-import { useEffect } from "react";
-
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-        if (!gtag.existsGaId) {
-            return;
-        }
-
-        const handleRouteChange = (path) => {
-            gtag.pageview(path);
-        };
-
-        Router.events.on("routeChangeComplete", handleRouteChange);
-        return () => {
-            Router.events.off("routeChangeComplete", handleRouteChange);
-        };
-    }, []);
-
     return <Component {...pageProps} />;
 }
