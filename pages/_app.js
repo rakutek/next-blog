165767f90349e0350e1e38c558c76cdad08b3d0e
rakutek <rakutek12@gmail.com>
2020-09-02 14:01:54 +0900

    GA設定した

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,24 @@ import "github-markdown-css/github-markdown.css";
 
 import "highlight.js/styles/a11y-dark.css";
 
+import * as gtag from "lib/gtag";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }) {
+    useEffect(() => {
+        if (!gtag.existsGaId) {
+            return;
+        }
+
+        const handleRouteChange = (path) => {
+            gtag.pageview(path);
+        };
+
+        Router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            Router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, []);
+
     return <Component {...pageProps} />;
 }
