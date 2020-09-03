87a4f2d11a0e7bfb0176df6a7d73a5e66065deee
rakutek <rakutek12@gmail.com>
2020-09-03 14:09:16 +0900

    import reactした

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import Router from "next/router";
-
 import * as gtag from "../lib/gtag";
+import React from "react";
 
 import "github-markdown-css";
 import "github-markdown-css/github-markdown.css";
-
 import "highlight.js/styles/a11y-dark.css";
 
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
